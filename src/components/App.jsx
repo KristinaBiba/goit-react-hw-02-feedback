64d34a3b5f78b5fdc3f47868ee39e8d5987bdec6
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2022-12-24 11:56:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react"
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleFidback = ({target}) => {
+    this.setState(prevState => { return {[target.id]: prevState[target.id] + 1,} })  
+  }
+ 
+  countTotalFeedback = () => {
+    return (Object.values(this.state)).reduce((acc, item) => {return acc + item}, 0)
+  }
+  countPositiveFeedbackPercentage = () => {
+    console.log(this.state.good);
+
+    return (Math.round(this.state.good / this.countTotalFeedback() * 100) ) || 0;
+  }
+
+  
+  render() {
+
+    const StateBtn = ({ btnName }) => {
+        return (
+          Object.keys(btnName).map(item => (<button type="button" key={item} id={item} onClick={this.handleFidback}>{item}</button>)) )
+    };
+    
+    const StatisticList = ({ statistics }) => {
+      console.log(statistics);
+      return (
+           (Object.entries(statistics)).map((statisticEl) => (<p key={statisticEl[0]}>{statisticEl[0]}: <b>{statisticEl[1]}</b></p>))
+      )
+    };
+
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+
+        <StateBtn btnName={this.state}></StateBtn>
+        
+        <h2>Statistics</h2>
+
+        <StatisticList statistics={this.state}></StatisticList>
+        <p>Total:  <b>{this.countTotalFeedback()}</b></p>
+        <p>positiveFeedback:  <b>{this.countPositiveFeedbackPercentage()}%</b></p>
+        
+      </>
+    );
+  };
 };
+
+
